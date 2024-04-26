e7652e34a0e4116b29de852a911574aa5787b192
이런우 <82223233+hw1186@users.noreply.github.com>
2024-04-26 15:43:50 +0900

    Add files via upload

---
+++ setup_linux.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# setup_linux.sh
+# chmod 755 setup_linux.sh
+
+echo "========================================================"
+echo "rsysloh set up"
+echo "========================================================"
+
+sudo apt update
+sudo apt install -y rsyslog
+sudo apt install systemd
+sudo apt install curl
+
+cat << EOF | sudo tee /etc/rsyslog.d/01-json-parser.conf
+template(name="json-template"
+  type="list") {
+    constant(value="{")
+      constant(value="\"@timestamp\":\"")     property(name="timereported" dateFormat="rfc3339")
+      constant(value="\",\"@version\":\"1")
+      constant(value="\",\"message\":\"")     property(name="msg" format="json")
+      constant(value="\",\"sysloghost\":\"")  property(name="hostname")
+      constant(value="\",\"formhost-ip\":\"")  property(name="fromhost-ip")
+      constant(value="\",\"severity\":\"")    property(name="syslogseverity-text")
+      constant(value="\",\"facility\":\"")    property(name="syslogfacility-text")
+      constant(value="\",\"programname\":\"") property(name="programname")
+      constant(value="\",\"procid\":\"")      property(name="procid")
+    constant(value="\"}\n")
+}
+EOF
+
+cat << EOF | sudo tee /etc/rsyslog.d/60-fluentd.conf
+*.*							@@localhost:5140;json-template
+EOF
+
+sudo service rsyslog stop
+
+sudo mv /run/rsyslogd.pid /run/rsyslogd.pid.backup
+
+sudo service rsyslog start
+
+curl -fsSL https://toolbelt.treasuredata.com/sh/install-ubuntu-jammy-fluent-package5.sh | sh
+
+echo "========================================================"
+echo "Starting install Fluentd"
+echo "========================================================"
+
+curl -fsSL https://toolbelt.treasuredata.com/sh/install-ubuntu-jammy-fluent-package5.sh | sh
+sudo systemctl start fluentd.service
+
+echo "========================================================"
+echo "setup Fluentd"
+echo "========================================================"
+
+sudo cp /etc/fluent/fluentd.conf /etc/fluent/fluentd.conf.bak
+
+cat << EOF | sudo tee /etc/fluent/fluentd.conf
+<source>
+  @type tcp
+  port 5140
+  bind 0.0.0.0
+  tag teiren.linux
+  <parse>
+    @type json
+  </parse>
+</source>
+
+<match teiren.linux>
+	@type http
+	endpoint http://3.36.119.174:8088/linux_log
+	json_array true
+	<format>
+	  @type json
+	</format>
+	<buffer>
+	  flush_interval 10s
+	</buffer>
+</match>
+EOF
+
+sudo systemctl restart fludentd
+
+chmod +x setup_linux.sh
